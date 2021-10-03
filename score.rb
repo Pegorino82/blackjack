474710e7f70e00d31a021d90f5ed364b09492cd3
Pegorino82 <eshkryabin@yandex.ru>
2021-10-03 16:38:46 +0300

    Fix Score

--- score.rb
+++ score.rb
@@ -8,11 +8,11 @@ module Score
       @cards |= []
       aces = @cards.select { |card| card.value == 'A' }
       others = @cards.select { |card| card.value.match(/[\dTJQK]/) }
-      sum = others.reduce(0) do |_, card|
-        + card.value.match(/[TJQK]/) ? 10 : card.value.to_i
+      sum = others.reduce(0) do |s, card|
+        card.value.to_i.zero? ? s + 10 : s + card.value.to_i
       end
-      aces.reduce(sum) do |_, _|
-        + sum + 11 > 21 ? 1 : 11
+      aces.reduce(sum) do |s, _|
+        s + 11 > 21 ? s + 1 : s + 11
       end
     end
   end
